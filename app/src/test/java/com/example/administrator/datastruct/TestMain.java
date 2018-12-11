358f375525cb6757f926ef2c4f8a39218df0cb58
wangweijun1 <wangweijun1@le.com>
2018-12-11 11:26:35 +0800

    test hashmpa

--- app/src/test/java/com/example/administrator/datastruct/TestMain.java
+++ app/src/test/java/com/example/administrator/datastruct/TestMain.java
@@ -1,5 +1,9 @@
 package com.example.administrator.datastruct;
 
+import org.junit.Test;
+
+import java.util.HashMap;
+
 public class TestMain {
 
 	public static void main(String[] args) {
@@ -12,6 +16,19 @@ public class TestMain {
 		for(int i = 0; i < linkedList.size; i++) {
 			System.out.print( i + ":" + linkedList.get(i) + "  ");
 		}
+	}
+
+
+
+	@Test
+	public void testHashMap() {
+		HashMap<User, String> hashmap = new HashMap<>();
+		String pre = hashmap.put(new User("xxx"), "xxx");
+		System.out.println(hashmap.size() + ", pre:"+pre);
+
+		pre = hashmap.put(new User("xxx"), "yyyy");
+		System.out.println(hashmap.size() + ", pre:"+pre);
+
 	}
 
 }
--- app/src/test/java/com/example/administrator/datastruct/User.java
+++ app/src/test/java/com/example/administrator/datastruct/User.java
@@ -7,5 +7,19 @@ public class User {
         this.name = name;
     }
 
-    User next;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        User user = (User) o;
+
+        return name.equals(user.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return name.hashCode();
+    }
 }
